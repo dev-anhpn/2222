a21909fb64d2889eec6f0ba15e1cc59c8e5729ec
= <namanh9917@gmail.com>
2019-06-19 09:33:55 +0700

    sfewr

---
+++ C-CuongHuynhPractice/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_CuongHuynhPractice
+{
+    enum Gioitinh
+    {
+        Male, 
+        Female
+    }
+    class Student
+    {
+        private string name;
+        private Gioitinh gender;
+        private string email;
+
+        public string Name { get => name; set => name = value; }
+        internal Gioitinh Gender { get => gender; set => gender = value; }
+        public string Email { get => email; set => email = value; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, Gioitinh gender, string email)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.email = email;
+        }
+
+        public void input()
+        {
+            Console.WriteLine("Nhap ten: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Nhap gender: ");
+            string gioitinh = Console.ReadLine();
+            if (gioitinh.Equals("Male"))
+            {
+                gender = Gioitinh.Male;
+            }
+            if (gioitinh.Equals("Female"))
+            {
+                gender = Gioitinh.Female;
+            }
+            Console.WriteLine("Nhap email: ");
+            Email = Console.ReadLine();
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Ten: "+name);
+            Console.WriteLine("Gender: "+ gender);
+            Console.WriteLine("Email: "+email);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new List<int>() { 1, 2, 3, 4 };
+
+            var le = from o in a where o % 2 != 0 select o;
+            //var le2 = a.Where(h => h %2! = 0);
+             var hsss = new List<hocsinh>
+             {
+
+             }
+            
+        }
+
+    }
+    class hocsinh
+    {
+        int id;
+        public int Id
+        {
+            get => id;
+        }
+    }
+}
